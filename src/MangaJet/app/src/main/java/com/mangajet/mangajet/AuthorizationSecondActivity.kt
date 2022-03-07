0191e2429db9ab5c707f8fe87a0ef1f80b640e96
AlDente_Noo <tymable@mail.ru>
2022-03-07 21:31:40 +0300

    Added ?second activities?. Some icons are changed. AB#26, AB#18, AB#28

---
+++ src/MangaJet/app/src/main/java/com/mangajet/mangajet/AuthorizationSecondActivity.kt
@@ -0,0 +1,11 @@
+package com.mangajet.mangajet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthorizationSecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authorization_second)
+    }
+}
---
+++ src/MangaJet/app/src/main/java/com/mangajet/mangajet/MangaReaderThirdActivity.kt
@@ -0,0 +1,11 @@
+package com.mangajet.mangajet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MangaReaderThirdActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manga_reader_third)
+    }
+}
