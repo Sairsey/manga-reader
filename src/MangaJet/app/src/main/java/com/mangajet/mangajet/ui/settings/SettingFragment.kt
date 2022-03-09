6f1ffab70f4a6ca35ad8fe865f10bac10b06baf0
AlDente_Noo <tymable@mail.ru>
2022-03-09 04:33:10 +0300

    All icons are changed - they're custom now. I didn't connect fragments, sorry, mr. Sachuck because I cannot understand, why somewhere we have fragments while in other places - activities. Gonna done it tomorra. AB#26, AB#18, AB#28

--- src/MangaJet/app/src/main/java/com/mangajet/mangajet/ui/settings/SettingFragment.kt
+++ src/MangaJet/app/src/main/java/com/mangajet/mangajet/ui/settings/SettingFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.mangajet.mangajet.R
 
 class SettingFragment : Fragment() {
