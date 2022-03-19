392b0482fb244b1cc7d2ed8ac25ab493c25b34b3
Vladimir <vladimir.parusov.vp1@gmail.com>
2022-03-19 20:13:19 +0300

    Added basic realization of MangaChapter AB#37

--- src/MangaJet/app/src/main/java/com/mangajet/mangajet/data/Manga.kt
+++ src/MangaJet/app/src/main/java/com/mangajet/mangajet/data/Manga.kt
@@ -2,12 +2,12 @@ package com.mangajet.mangajet.data
 
 // Class that represents one specific manga, stores info about it(name, author, genre...) and chapters of this manga
 class Manga {
-    val i = 1// To fool detect
     val id : String
+    val library : AbstractLibrary
 
     // Constructor for Libraries
     constructor(library: AbstractLibrary, id: String) {
-        library.getURL()
+        this.library = library
         this.id = id
     }
 }
--- src/MangaJet/app/src/main/java/com/mangajet/mangajet/data/MangaChapter.kt
+++ src/MangaJet/app/src/main/java/com/mangajet/mangajet/data/MangaChapter.kt
@@ -1,12 +1,34 @@
 package com.mangajet.mangajet.data
 
+import org.json.JSONObject
+import javax.net.ssl.ManagerFactoryParameters
+
 // Class that represents one chapter of specific, stores pages of this manga
 class MangaChapter {
-    val i = 1// To fool detect
+    public var manga : Manga      // Manga from which this chapter created
+    public var id : String        // Id of this Chapter
+    public var lastViewedPage = 0 // Number of last viewed page
+    private var pagesNumber = -1  // Number of pages in this chapter
 
     // Constructor for Libraries
     constructor(manga: Manga, id: String) {
-        manga.hashCode()
-        id + ""
+        this.manga = manga
+        this.id = id
+    }
+
+    // Function for safely retrieving amount of pages
+    public fun getPagesNum() : Int {
+        if (pagesNumber == -1)
+            pagesNumber = manga.library.getChaptersNumOfPages(manga.id, id)
+        return pagesNumber
+    }
+
+    // Function for safely retrieving amount of pages
+    public fun getPage(pageNumber : Int) : MangaPage {
+        if (pagesNumber == -1)
+            getPagesNum()
+        if (pageNumber < 0 || pageNumber >= pagesNumber)
+            throw Exception("Bad page index")
+        return manga.library.getChapterPage(manga.id, id, pageNumber)
     }
 }
