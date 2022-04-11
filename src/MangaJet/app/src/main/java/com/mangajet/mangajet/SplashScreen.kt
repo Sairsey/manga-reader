bba44d800ef697bc512d7903a0607114c519c245
AlDente_Noo <tymable@mail.ru>
2022-04-11 23:02:25 +0300

    Added splash screen AB#71

---
+++ src/MangaJet/app/src/main/java/com/mangajet/mangajet/SplashScreen.kt
@@ -0,0 +1,33 @@
+package com.mangajet.mangajet
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+
+@Suppress("DEPRECATION")
+class SplashScreen : AppCompatActivity() {
+    companion object {
+        const val DELAY = 3000
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        // This is used to hide the status bar and make
+        // the splash screen as a full screen activity.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        // we used the postDelayed(Runnable, time) method
+        // to send a message with a delayed time.
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, DELAY.toLong()) // 3000 is the delayed time in milliseconds.
+    }
+}
