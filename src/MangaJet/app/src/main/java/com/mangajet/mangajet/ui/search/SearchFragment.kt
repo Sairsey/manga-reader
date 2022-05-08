6c7ed4182c3c472f797e8cdc283ca0afd850aea9
AS2 <alex.sachuk@yandex.ru>
2022-05-08 03:49:48 +0300

    Added example and temporary search queries history AB#122

--- src/MangaJet/app/src/main/java/com/mangajet/mangajet/ui/search/SearchFragment.kt
+++ src/MangaJet/app/src/main/java/com/mangajet/mangajet/ui/search/SearchFragment.kt
@@ -1,14 +1,18 @@
 package com.mangajet.mangajet.ui.search
 
-import com.mangajet.mangajet.R
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.database.Cursor
+import android.database.MatrixCursor
 import android.os.Bundle
+import android.provider.BaseColumns
 import android.view.LayoutInflater
-import android.view.ViewGroup
-import android.view.ContextMenu
 import android.view.MenuItem
 import android.view.View
+import android.view.ViewGroup
+import android.widget.CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER
 import android.widget.SearchView
+import android.widget.SimpleCursorAdapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProvider
@@ -16,6 +20,7 @@ import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.mangajet.mangajet.MangaJetApp
 import com.mangajet.mangajet.MangaListAdapter
+import com.mangajet.mangajet.R
 import com.mangajet.mangajet.aboutmanga.AboutMangaActivity
 import com.mangajet.mangajet.databinding.SearchFragmentBinding
 import com.mangajet.mangajet.log.Logger
@@ -55,24 +60,57 @@ class SearchFragment : Fragment() {
                 val intent = Intent(frag, AboutMangaActivity::class.java)
                 MangaJetApp.currentManga = searchViewModel.mangas[id.toInt()]
                 startActivityForResult(intent, MangaJetApp.ABOUT_MANGA_CALLBACK)}
+            val list = searchViewModel.suggestionsStrings!!.toMutableList()
+            list.add(p0)
+            searchViewModel.suggestionsStrings = list.toTypedArray()
 
             return true
         }
 
-        override fun onQueryTextChange(p0: String?): Boolean {
+        override fun onQueryTextChange(query: String?): Boolean {
+            val mAdapter = binding.searchView.suggestionsAdapter
+
+            val cursor = MatrixCursor(arrayOf(BaseColumns._ID, "Query"))
+            query?.let {
+                searchViewModel.suggestionsStrings!!.forEachIndexed { index, suggestion ->
+                    if (suggestion.contains(query, true))
+                        cursor.addRow(arrayOf(index, suggestion))
+                }
+            }
+
+            mAdapter.changeCursor(cursor)
             return false
         }
-
     }
 
-    override fun onCreateContextMenu(
-        menu: ContextMenu,
-        v: View,
-        menuInfo: ContextMenu.ContextMenuInfo?
-    ) {
-        super.onCreateContextMenu(menu, v, menuInfo)
-    }
+    private fun initSuggestionAdapter() {
+        // init data with suggestions first
+        searchViewModel.initSuggestionData()
 
+        // init adapter
+        val from = arrayOf("Query")
+        val to = intArrayOf(R.id.search_suggestion_element)
+        val mAdapter = SimpleCursorAdapter(activity, R.layout.search_item, null,
+            from, to, FLAG_REGISTER_CONTENT_OBSERVER)
+        binding.searchView.suggestionsAdapter = mAdapter
+
+        // set suggestions listener
+        binding.searchView.setOnSuggestionListener(object: SearchView.OnSuggestionListener {
+            override fun onSuggestionSelect(position: Int): Boolean {
+                return false
+            }
+
+            @SuppressLint("Range")
+            override fun onSuggestionClick(position: Int): Boolean {
+                val cursor = binding.searchView.suggestionsAdapter.getItem(position) as Cursor
+                val selection = cursor.getString(cursor.getColumnIndex(from[0]))
+                binding.searchView.setQuery(selection, true)
+
+                // Do something with selection
+                return true
+            }
+        })
+    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
@@ -103,6 +141,7 @@ class SearchFragment : Fragment() {
         setHasOptionsMenu(true)
     }
 
+    @SuppressLint("RestrictedApi")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -116,6 +155,7 @@ class SearchFragment : Fragment() {
         _binding = SearchFragmentBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+
         // init toolbar
         mToolbar = binding.searchToolbar
         mToolbar?.inflateMenu(R.menu.search_menu)
@@ -127,9 +167,12 @@ class SearchFragment : Fragment() {
         binding.noResultLayout.visibility = View.VISIBLE
         binding.progressBar.hide()
 
-        // init list with mangas
+
+        // init search view and list with mangas
         var searchView = binding.searchView
         activity?.let {
+            initSuggestionAdapter()
+            searchViewModel.setSomeRandomPopularHint(binding)
             searchView.setOnQueryTextListener(OnQueryTextListener(it))
         }
 
@@ -175,6 +218,7 @@ class SearchFragment : Fragment() {
         super.onDestroyView()
         binding.searchView.setQuery("", false)
         searchViewModel.job?.cancel()
+        searchViewModel.hintJob?.cancel()
         _binding = null
     }
 }
